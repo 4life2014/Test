c4e4e6f3631ce8346e6b2a49339ed255f831110b
duj <shalee@shaleetekiMacBook-Pro.local>
2014-10-29 15:53:17 +0800

    ddd

---
+++ xx001/xx001/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xx001
+//
+//  Created by duj on 14-10-29.
+//  Copyright (c) 2014年 duj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xx001/xx001/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  xx001
+//
+//  Created by duj on 14-10-29.
+//  Copyright (c) 2014年 duj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
