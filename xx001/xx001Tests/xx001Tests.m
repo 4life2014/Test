8a092519fa0b3df70c51459c06ab8a8e02208daa
duj <shalee@shaleetekiMacBook-Pro.local>
2014-10-29 15:56:57 +0800

    ttttttt

---
+++ xx001/xx001Tests/xx001Tests.m
@@ -0,0 +1,34 @@
+//
+//  xx001Tests.m
+//  xx001Tests
+//
+//  Created by duj on 14-10-29.
+//  Copyright (c) 2014年 duj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xx001Tests : XCTestCase
+
+@end
+
+@implementation xx001Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
